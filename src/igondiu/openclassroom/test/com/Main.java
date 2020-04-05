f1add2a7270943c87f1404ebd9e8b915f4ba2091
igondiu <ion.gondiu@ibm.com>
2020-04-05 04:51:57 +0200

    commit 2

--- src/igondiu/openclassroom/test/com/Main.java
+++ src/igondiu/openclassroom/test/com/Main.java
@@ -3,6 +3,41 @@ package igondiu.openclassroom.test.com;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Ville v = new Ville();
+        Ville paris  = new Ville( "Paris","France", 9000000);
+        Ville v1 = new Ville("Marseille", "France", 123456);
+        Ville v2 = new Ville("Rio", "Brésil", 321654);
+
+
+        System.out.println("\n v = "+v.getNom()+" ville de  "+v.getNombreHabitants()+ " habitants se situant en "+v.getNomPays());
+        System.out.println(" v1 = "+v1.getNom()+" ville de  "+v1.getNombreHabitants()+ " habitants se situant en "+v1.getNomPays());
+        System.out.println(" v2 = "+v2.getNom()+" ville de  "+v2.getNombreHabitants()+ " habitants se situant en "+v2.getNomPays()+"\n\n");
+
+/*
+  Nous allons interchanger les Villes v1 et v2
+  tout ça par l'intermédiaire d'un autre objet Ville.
+*/
+        Ville temp = new Ville();
+        temp = v1;
+        v1 = v2;
+        v2 = temp;
+
+        System.out.println(" v1 = "+v1.getNom()+" ville de  "+v1.getNombreHabitants()+ " habitants se situant en "+v1.getNomPays());
+        System.out.println(" v2 = "+v2.getNom()+" ville de  "+v2.getNombreHabitants()+ " habitants se situant en "+v2.getNomPays()+"\n\n");
+
+/*
+  Nous allons maintenant interchanger leurs noms
+  cette fois par le biais de leurs mutateurs.
+*/
+        v1.setNom("Hong Kong");
+        v2.setNom("Djibouti");
+
+        System.out.println(" v1 = "+v1.getNom()+" ville de  "+v1.getNombreHabitants()+ " habitants se situant en "+v1.getNomPays());
+        System.out.println(" v2 = "+v2.getNom()+" ville de  "+v2.getNombreHabitants()+ " habitants se situant en "+v2.getNomPays()+"\n\n");
+
     }
+    Ville V = new Ville("Lyon", "France", 654);
+    Ville V2 = new Ville("Lille", "France",123);
+
+    V.comparer(V2);
 }
--- src/igondiu/openclassroom/test/com/Ville.java
+++ src/igondiu/openclassroom/test/com/Ville.java
@@ -1,11 +1,77 @@
 package igondiu.openclassroom.test.com;
 
 public class Ville {
-    String nomVille;
-    String nomPays;
-    int nbHabitants;
+    private String nomVille;
+    private String nomPays;
+    private int nbHabitants;
+    private char categorie;
 
     public Ville(){
-        System.out.println("Création de la ville");
+        System.out.println("Création d'une ville vide...");
+        this.nomVille = "inconnu";
+        this.nomPays = "inconnu";
+        this.nbHabitants = 0;
+        this.setCategorie();
+    }
+
+    public Ville(String nomVille, String nomPays, int nbHabitants){
+        System.out.println("Création de la ville "+nomVille+ " est en cours...");
+        this.nbHabitants = nbHabitants;
+        this.nomPays = nomPays;
+        this.nomVille = nomVille;
+        this.setCategorie();
+    }
+
+    //Retourne la description de la ville
+    public String decrisToi(){
+        return "\t"+this.nomVille+" est une ville de "+this.nomPays+ ", elle comporte : "+this.nbHabitants+" habitant(s) => elle est donc de catégorie : "+this.categorie;
+    }
+
+
+    //Retourne une chaîne de caractères selon le résultat de la comparaison
+    public String comparer(Ville v1){
+        String str;
+
+        if (v1.getNombreHabitants() > this.nbHabitants)
+            str = v1.getNom()+" est une ville plus peuplée que "+this.nomVille;
+
+        else
+            str = this.nomVille+" est une ville plus peuplée que "+v1.getNom();
+
+        return str;
+    }
+
+    private void setCategorie(){
+        if(this.nbHabitants < 1000000){
+            this.categorie = 'A';
+        }else if(this.nbHabitants > 1000000 && this.nbHabitants < 6000000){
+            this.categorie = 'B';
+        }else{
+            this.categorie = 'C';
+        }
+    }
+
+    public String getNom() {
+        return nomVille;
+    }
+
+    public void setNom(String nomVille) {
+        this.nomVille = nomVille;
+    }
+
+    public String getNomPays() {
+        return nomPays;
+    }
+
+    public void setNomPays(String nomPays) {
+        this.nomPays = nomPays;
+    }
+
+    public int getNombreHabitants() {
+        return nbHabitants;
+    }
+
+    public void setNbHabitants(int nbHabitants) {
+        this.nbHabitants = nbHabitants;
     }
 }
